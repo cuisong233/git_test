c4d5c3c177c2e5cb5665dd28f7894ff2aaa8d406
cuisong233 <qingsong233@hotmail.com>
2022-12-12 20:01:16 +0800

    初始化的代码+tensorboard

--- main.py
+++ main.py
@@ -6,32 +6,85 @@ import os
 import tqdm
 import datetime
 import uuid
+import torchvision
+import model
+import torch
+import torch.optim as optim
+from torchvision import transforms
+import torch.utils.data as Data
+from torch.utils.tensorboard import SummaryWriter
 
-# 学学怎么用yaml
+# tensorboard --logdir=runs --port=5200
 
-os.environ['VAR_NAME']=sys.argv[1]                       # 设置与实验相关的环境变量（如CUDA_VISIBLE_DEVICES）
+writer = SummaryWriter('runs/fashion_mnist_experiment_1')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# os.environ['VAR_NAME']=sys.argv[1]                       # 设置与实验相关的环境变量（如CUDA_VISIBLE_DEVICES）
 experiment_id = str(uuid.uuid1())[:8]                    # 生成本次实验的UUID
-experiment_name = name                                   # 设置描述本次实验的名称
+experiment_name = '初始版本'                                   # 设置描述本次实验的名称
 logger=dict()                                            # 用字典保存代码、进程ID、配置参数、开始时间、训练时产生的数据等日志信息
 logger['experiment_id'] = experiment_id                  # 保存本次实验的UUID
 logger['experiment_name'] = experiment_name              # 保存本次实验的名称
 logger['code']=inspect.getsource(sys.modules[__name__])  # 保存本次实验代码
 logger['pid']=os.getpid()                                # 保存本次实验进程PID
-logger['config']=config                                  # 保存配置参数
+logger['config']= "??"                                  # 保存配置参数
 logger['datetime']=str(datetime.datetime.now())          # 保存训练开始时间
 logger['loss'] = []                                      # 保存loss日志
 logger['info'] = []                                      # 保存其他日志信息
 logger['env_vars'] = os.environ                          # 保存相关环境变量
 batch_cnt = 0
 log_freq = 100
+
+""" 超参数 """
+LR = 0.001
+EPOCHS = 100
+BATCHSIZE = 16
+
+
+""" 数据准备 """
+# 定义 数据集的变换
+img_transform = transforms.Compose([
+    transforms.Resize(64),  # 调整到 64*64 大小
+    transforms.ToTensor(),  # 转为tensor
+    transforms.Normalize(mean=0.5, std=0.5)
+]   )
+# 设置mnist数据
+train_data = torchvision.datasets.MNIST(
+    root='./mnist',
+    train=True,
+    transform=img_transform
+)
+train_loader = Data.DataLoader(dataset=train_data, batch_size=BATCHSIZE, shuffle=True, drop_last=True)
+
+
+""" 模型准备 """
+model = model.Net().to(device)
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=LR, momentum=0.9)
+
+
+
+
+
+""" 训练 """
 try:
-    for i in tqdm.tqdm(range(N)):
-        for x,y in dataset:
-            loss=model.fit(x, y) # 反向传播
+    for epoch in range(EPOCHS):
+        train_bar = tqdm.tqdm(train_loader)
+        for step, (x, y) in enumerate(train_bar):
+            x,y = x.to(device), y.to(device)
+            out = model(x)
+            loss = criterion(out, y)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
             logger['loss'].append(loss)
-            logger['info'].append(info)
+            # logger['info'].append(info)
             batch_cnt += 1
+            train_bar.set_description(desc=f"[{epoch}/{epoch - 1}] [Train] [Loss : {loss:.4f},")
+
             if batch_cnt % log_freq == 0: # 每log_freq个batch保存一次日志
+                writer.add_scalar("training loss", loss, epoch * len(train_loader) + step)
                 with open(experiment_name + experiment_id + '.log','w') as f:
                     f.write(yaml.dump(logger, Dumper=yaml.CDumper)) # 使用yaml保存日志
 except KeyboardInterrupt:
@@ -39,4 +92,5 @@ except KeyboardInterrupt:
 except Exception:
     print(traceback.format_exc())
 finally:
-    postprocess(model) # 训练结束后处理部分，比如保存模型权重等信息到磁盘
+    pass
+    # postprocess(model) # 训练结束后处理部分，比如保存模型权重等信息到磁盘
